5fb6981e342017763665985d86da09458e63d536
Kailynnelopez <kailynnelopez@gmail.com>
2022-12-03 14:06:22 -0800

    Implement Under Construction View

---
+++ src/components/Construction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Construction() {
+    return (
+        <div className='py-5 text-center'>
+            <h1 className='text-center text-black'>
+                This Website is Currently Under Construction
+            </h1>
+        </div>
+    )
+}
+
+export default Construction;
