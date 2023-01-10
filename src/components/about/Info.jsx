53c32907db81c15b0d997fa5cb9886042fbeba8a
Kailynnelopez <kailynnelopez@gmail.com>
2023-01-09 19:53:22 -0800

    restored changes

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+      <i class='bx bxs-school about__icon'></i>
+
+        <h3 className="about__title">Education</h3>
+        <span className="about__subtitle">Biola Univsersity</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">2 internships</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-bulb about__icon"></i>
+
+        <h3 className="about__title">Projects</h3>
+        <span className="about__subtitle">25+</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/tailwind.css'
+import "./App.css";
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Work from './components/Portfolio/Work';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+// import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+    <Header />
+
+    <main className='main'>
+      <Home />
+      <Work />
+      <About />
+      <Contact />
+    </main>
+
+    <Footer />
+    <ScrollUp />
+    </>
+
   </React.StrictMode>
 )
  
