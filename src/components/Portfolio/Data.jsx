2eb5944c6ae3675266821c0ca4e04f2d337a62cd
Kailynnelopez <kailynnelopez@gmail.com>
2023-05-17 09:41:04 -0700

    Added new projects

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -3,12 +3,32 @@ import PregProj_Cover from "../../assets/PregWeb.jpg";
 import GCC_Cover from "../../assets/gcccover.png";
 import NCC_Cover from "../../assets/NewCityCleaningCover.png";
 import PaceApp_Cover from "../../assets/Pace-APP-Cover.jpg"
+import QR_Cover from "../../assets/qrComponent.png"
+import ResultsSummary_Cover from "../../assets/ResultsSummary.png"
 
 
 
 export const projectsData = [
   {
     id: 1,
+    image: QR_Cover,
+    title: "QR Code Component",
+    category: "development",
+    cta: "View Repository",
+    link:"https://github.com/kailynnelopez/qr-code-component.git",
+    icon: "bx bx-right-arrow-alt work__button-icon",
+  },
+  {
+    id: 2,
+    image: ResultsSummary_Cover,
+    title: "Results Summary Component",
+    category: "development",
+    cta: "View Repository",
+    link:"https://github.com/kailynnelopez/results-summary-component",
+    icon: "bx bx-right-arrow-alt work__button-icon",
+  },
+  {
+    id: 3,
     image: Pace_Cover,
     title: "PACE - Reading Planning Generator",
     category: "development",
@@ -17,16 +37,7 @@ export const projectsData = [
     icon: "bx bx-right-arrow-alt work__button-icon",
   },
   {
-    id: 2,
-    image: GCC_Cover,
-    title: "Golden Coast Construction Website",
-    category: "design",
-    cta: "Coming Soon",
-    link: "src/assets/gcccover.png",
-    icon: " ",
-  },
-  {
-    id: 3,
+    id: 4,
     image: NCC_Cover,
     title: "New City Cleaning Service Website",
     category: "design",
@@ -35,7 +46,7 @@ export const projectsData = [
     icon: "bx bx-right-arrow-alt work__button-icon",
   },
   {
-    id: 4,
+    id: 5,
     image: PaceApp_Cover,
     title: "PACE - App",
     category: "design",
@@ -44,13 +55,22 @@ export const projectsData = [
     icon: "bx bx-right-arrow-alt work__button-icon",
   },
   {
-    id: 5,
+    id: 6,
     image: PregProj_Cover,
     title: "Pregnancy Support Center",
     category: "design",
     cta: "Case Study",
     link: "https://kailynnelopez.notion.site/Pregnancy-Support-Center-01388ce843a04e62945f4f74c137fa12",
     icon: "bx bx-right-arrow-alt work__button-icon",
+  },
+  {
+    id: 7,
+    image: GCC_Cover,
+    title: "Golden Coast Construction Website",
+    category: "design",
+    cta: "Coming Soon",
+    link: "src/assets/gcccover.png",
+    icon: " ",
   },
 ];
 
